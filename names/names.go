f4417d311e530f6032a4f2132473dac6d6f89d87
Jeff Andersen <jeffandersen@gmail.com>
2017-11-02 16:08:55 -0300

    Remove additional entropy suffix from label

--- names/names.go
+++ names/names.go
@@ -4,15 +4,12 @@ import (
 	"encoding/binary"
 	"strings"
 
-	"github.com/manifoldco/go-base32"
-
 	"github.com/manifoldco/go-manifold"
 	"github.com/manifoldco/go-manifold/names/data"
 )
 
 const (
-	entropy    = 16 * 8 // in bits
-	base32Bits = 25     // Enough to fill 5 characters exactly.
+	entropy = 16 * 8 // in bits
 )
 
 var aShare, cShare, sShare int
@@ -22,27 +19,24 @@ func init() {
 	cShare = bitsNeeded(len(data.Colors))
 	sShare = bitsNeeded(len(data.Shapes))
 
-	if aShare+cShare+sShare+base32Bits > entropy {
+	if aShare+cShare+sShare > entropy {
 		panic("Word lists are now too big")
 	}
 }
 
 // New returns a generated name based on the provided id, and its matching label.
 // Names are title cased with spaces between words.
-// Labels are lowercased with hyphens between words, and 5 trailing characters
-// of base32 encoded data from the id.
+// Labels are lowercased with hyphens between words
 func New(id manifold.ID) (string, string) {
 	idBytes := id[2:]
 
 	offset := 0
 	adj, offset := fetchWord(idBytes, data.Adjectives, offset, aShare)
 	color, offset := fetchWord(idBytes, data.Colors, offset, cShare)
-	shape, offset := fetchWord(idBytes, data.Shapes, offset, sShare)
-
-	num := fetchNum(idBytes, offset)
+	shape, _ := fetchWord(idBytes, data.Shapes, offset, sShare)
 
 	name := strings.Title(adj + " " + color + " " + shape)
-	label := strings.Replace(strings.ToLower(name), " ", "-", -1) + "-" + num
+	label := strings.Replace(strings.ToLower(name), " ", "-", -1)
 	return name, label
 }
 
@@ -65,20 +59,6 @@ func fetchWord(idBytes []byte, wordList []string, offset, bitShare int) (string,
 	}
 
 	return wordList[idx], offset + bytesNeeded
-}
-
-func fetchNum(idBytes []byte, offset int) string {
-	mask := byte(0xff)
-	bytesNeeded := base32Bits / 8
-	if rem := base32Bits % 8; rem > 0 {
-		bytesNeeded++
-		mask <<= uint(8 - rem)
-	}
-
-	vBytes := idBytes[offset : offset+bytesNeeded]
-	vBytes[len(vBytes)-1] &= mask
-
-	return base32.EncodeToString(vBytes)[:5]
 }
 
 func bitsNeeded(val int) int {
