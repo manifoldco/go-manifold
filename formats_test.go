e262a63f5361bdea8bb5a72132ac9d1022284ada
Tim Speed <tim-speed@users.noreply.github.com>
2018-07-09 11:42:00 -0300

    Added new type for FeatureValueLabels (#62)
    This will be used in Marketplace and across clients, including Grafton
    
    Relates manifoldco/engineering#4858

--- formats_test.go
+++ formats_test.go
@@ -53,3 +53,46 @@ func TestCode(t *testing.T) {
 		}
 	})
 }
+
+func TestFeatureValueLabel(t *testing.T) {
+	t.Run("errors on invalid feature value label", func(t *testing.T) {
+		l := FeatureValueLabel("BluesClues")
+		err := l.Validate(nil)
+		if err == nil {
+			t.Error("Expected an error")
+		}
+		l = FeatureValueLabel("blues clues")
+		err = l.Validate(nil)
+		if err == nil {
+			t.Error("Expected an error")
+		}
+		l = FeatureValueLabel("moosejuice!")
+		err = l.Validate(nil)
+		if err == nil {
+			t.Error("Expected an error")
+		}
+
+		_, ok := err.(*Error)
+		if !ok {
+			t.Error("Expected a manifold Error")
+		}
+	})
+
+	t.Run("does not error on valid feature value label", func(t *testing.T) {
+		l := FeatureValueLabel("t2.micro")
+		err := l.Validate(nil)
+		if err != nil {
+			t.Errorf("Unexpected Error: %s", err)
+		}
+		l = FeatureValueLabel("fat-cats")
+		err = l.Validate(nil)
+		if err != nil {
+			t.Errorf("Unexpected Error: %s", err)
+		}
+		l = FeatureValueLabel("orange_soda")
+		err = l.Validate(nil)
+		if err != nil {
+			t.Errorf("Unexpected Error: %s", err)
+		}
+	})
+}
