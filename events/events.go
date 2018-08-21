09f0e183f2e23c5b8cda7080711cdc1b127069ab
Luiz Branco <me@luizbranco.com>
2018-08-21 13:24:05 -0300

    Add fix

--- events/events.go
+++ events/events.go
@@ -600,7 +600,7 @@ func analyticsProperties(s interface{}) map[string]interface{} {
 
 		switch field.Kind() {
 		case reflect.Ptr:
-			if field.IsNil() {
+			if field.IsNil() || !field.CanInterface() {
 				continue
 			}
 
