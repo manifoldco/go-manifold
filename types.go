f1257004c5d0a68373869a0ab3f4f1c37f2f0368
Tim Speed <tim@manifold.co>
2018-08-29 11:49:25 -0300

    Type for Metadata
    This adds a type to store metadata.
    This will be used for provision operations and resources initially.
    
    Relates manifoldco/engineering#5801

--- types.go
+++ types.go
@@ -1,18 +1,75 @@
 package manifold
 
-// FeatureMap stores the selected feature values for a Manifold resource
-type FeatureMap map[string]interface{}
+import (
+	"encoding/json"
+	"reflect"
 
-// Equals checks the equality of another FeatureMap against this one
-func (f FeatureMap) Equals(fm FeatureMap) bool {
-	if len(f) != len(fm) {
+	"github.com/pkg/errors"
+)
+
+func dataMapEquals(a map[string]interface{}, b map[string]interface{}) bool {
+	if len(a) != len(b) {
 		return false
 	}
-	for k, v := range f {
-		if val, ok := fm[k]; !ok || val != v {
+	for k, v := range a {
+		if val, ok := b[k]; !ok || val != v {
 			return false
 		}
 	}
 
 	return true
+}
+
+// FeatureMap stores the selected feature values for a Manifold resource
+type FeatureMap map[string]interface{}
+
+// Equals checks the equality of another FeatureMap against this one
+func (f FeatureMap) Equals(fm FeatureMap) bool {
+	return dataMapEquals(f, fm)
+}
+
+// Metadata stores Metadata for a Manifold resource
+type Metadata map[string]interface{}
+
+// MetadataMaxSize defines the max size of the metadata JSON in bytes
+const MetadataMaxSize = 1024
+
+// Equals checks the equality of another Metadata against this one
+func (m Metadata) Equals(md Metadata) bool {
+	return dataMapEquals(m, md)
+}
+
+// Validate validates this Metadata
+func (m Metadata) Validate(_ interface{}) error {
+	for k, v := range m {
+		// Make sure key is a label
+		if err := Label(k).Validate(nil); err != nil {
+			return errors.Errorf("Key '%s' is not a valid Manifold Label", k)
+		}
+		// Make sure value is an allowed type
+		switch t := v.(type) {
+		case string, bool, float32, float64, int, int8, int16, int32, int64, uint,
+			uint8, uint16, uint32, uint64, json.Number:
+			continue
+		case map[string]interface{}:
+			if err := Metadata(t).Validate(nil); err != nil {
+				return err
+			}
+		case Metadata:
+			if err := t.Validate(nil); err != nil {
+				return err
+			}
+		default:
+			typ := reflect.TypeOf(v)
+			return errors.Errorf("'%s' is an invalid field value type for Metadata", typ.Name())
+		}
+	}
+	// Make sure total length isn't too long
+	b, _ := json.Marshal(m)
+	bLen := len(b)
+	if bLen > MetadataMaxSize {
+		return errors.Errorf("%d is %d bytes larger than the Metadata size limit of %d",
+			bLen, MetadataMaxSize-bLen, MetadataMaxSize)
+	}
+	return nil
 }
--- types_test.go
+++ types_test.go
@@ -1,6 +1,7 @@
 package manifold
 
 import (
+	"encoding/json"
 	"testing"
 )
 
@@ -48,4 +49,124 @@ func TestFeatureMap(t *testing.T) {
 			t.Error("A and B are equal, but aren't.")
 		}
 	})
+}
+
+func TestMetadata(t *testing.T) {
+	t.Run("different Metadata are not equal", func(t *testing.T) {
+		a := Metadata{
+			"abra":       1,
+			"bulbasaur":  "TWO",
+			"charmander": false,
+		}
+		b := Metadata{
+			"abra":       2,
+			"bulbasaur":  "one",
+			"charmander": true,
+		}
+		c := Metadata{
+			"abra":      1,
+			"bulbasaur": "TWO",
+		}
+
+		if a.Equals(b) {
+			t.Error("A and B are not equal, but are.")
+		}
+		if b.Equals(c) {
+			t.Error("B and C are not equal, but are.")
+		}
+		if a.Equals(c) {
+			t.Error("A and C are not equal, but are.")
+		}
+	})
+
+	t.Run("Metadata with the same values are equal", func(t *testing.T) {
+		a := Metadata{
+			"abra":       1,
+			"bulbasaur":  "TWO",
+			"charmander": false,
+		}
+		b := Metadata{
+			"abra":       1,
+			"bulbasaur":  "TWO",
+			"charmander": false,
+		}
+
+		if !a.Equals(b) {
+			t.Error("A and B are equal, but aren't.")
+		}
+	})
+
+	t.Run("Expected Metadata is considered valid", func(t *testing.T) {
+		mds := []Metadata{
+			Metadata{
+				"abra":       1,
+				"bulbasaur":  "TWO",
+				"charmander": false,
+			},
+			Metadata{
+				"abra":       1,
+				"bulbasaur":  "TWO",
+				"charmander": false,
+				"subdata": Metadata{
+					"abra":       1,
+					"bulbasaur":  "TWO",
+					"charmander": false,
+				},
+			},
+			Metadata{
+				"abra":       1,
+				"bulbasaur":  "TWO",
+				"charmander": false,
+				"subdata": map[string]interface{}{
+					"abra":       1,
+					"bulbasaur":  "TWO",
+					"charmander": false,
+				},
+			},
+		}
+
+		for _, m := range mds {
+			if err := m.Validate(nil); err != nil {
+				t.Error(err)
+			}
+		}
+	})
+
+	t.Run("Expected Metadata is considered invalid", func(t *testing.T) {
+		mds := []Metadata{
+			Metadata{
+				"$$$BADLABEL$$": 1,
+			},
+			Metadata{
+				"bad-value-type": json.Encoder{},
+			},
+			Metadata{
+				"nested-value-err": Metadata{
+					"$$$BADLABEL$$": 1,
+				},
+			},
+			Metadata{
+				"nested-value-err": map[string]interface{}{
+					"$$$BADLABEL$$": 1,
+				},
+			},
+			Metadata{
+				"json-too-big": makeBigString(MetadataMaxSize),
+			},
+		}
+
+		for _, m := range mds {
+			if err := m.Validate(nil); err == nil {
+				t.Errorf("Expected error for Metadata case: %v", m)
+			}
+		}
+	})
+}
+
+func makeBigString(size int) string {
+	b := make([]byte, size)
+	for i := 0; i < size; i++ {
+		b[i] = 'a'
+	}
+	return string(b)
 }
