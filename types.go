e6119edbb3a7c23dec9c2441b75e25dbc956aa06
Tim Speed <tim@manifold.co>
2018-08-30 16:59:51 -0300

    Implemented changes as discussed

--- types.go
+++ types.go
@@ -2,17 +2,21 @@ package manifold
 
 import (
 	"encoding/json"
-	"reflect"
 
+	"github.com/manifoldco/go-manifold/number"
 	"github.com/pkg/errors"
 )
 
-func dataMapEquals(a map[string]interface{}, b map[string]interface{}) bool {
-	if len(a) != len(b) {
+// FeatureMap stores the selected feature values for a Manifold resource
+type FeatureMap map[string]interface{}
+
+// Equals checks the equality of another FeatureMap against this one
+func (f FeatureMap) Equals(fm FeatureMap) bool {
+	if len(f) != len(fm) {
 		return false
 	}
-	for k, v := range a {
-		if val, ok := b[k]; !ok || val != v {
+	for k, v := range f {
+		if val, ok := fm[k]; !ok || val != v {
 			return false
 		}
 	}
@@ -20,23 +24,164 @@ func dataMapEquals(a map[string]interface{}, b map[string]interface{}) bool {
 	return true
 }
 
-// FeatureMap stores the selected feature values for a Manifold resource
-type FeatureMap map[string]interface{}
+// MetadataValue stores MetadataValue for a Manifold resource
+type MetadataValue struct {
+	Type  string      `json:"type"`
+	Value interface{} `json:"value"`
+}
 
-// Equals checks the equality of another FeatureMap against this one
-func (f FeatureMap) Equals(fm FeatureMap) bool {
-	return dataMapEquals(f, fm)
+// Equals checks the equality of another MetadataValue against this one
+func (m *MetadataValue) Equals(md MetadataValue) bool {
+	return m.Type == md.Type && m.Value == md.Value
+}
+
+func (m *MetadataValue) tryCastFields() error {
+	switch m.Type {
+	case "string":
+		_, ok := m.Value.(string)
+		if !ok {
+			return errors.New("Expected value to be a string but it was not")
+		}
+	case "bool":
+		_, ok := m.Value.(bool)
+		if !ok {
+			return errors.New("Expected value to be a boolean but it was not")
+		}
+	case "int":
+		val, err := number.ToInt64(m.Value)
+		if err != nil {
+			return errors.Errorf(
+				"Expected value to be castable to int64 but it was not: %s", err.Error())
+		}
+		m.Value = val
+	case "float":
+		val, err := number.ToFloat64(m.Value)
+		if err != nil {
+			return errors.Errorf(
+				"Expected value to be castable to float64 but it was not: %s", err.Error())
+		}
+		m.Value = val
+	case "object":
+		val, ok := m.Value.(Metadata)
+		if !ok {
+			valMap, ok := m.Value.(map[string]interface{})
+			if !ok {
+				return errors.Errorf("Expected value to be a valid metadata object but it was not")
+			}
+			val = Metadata{}
+			for k, v := range valMap {
+				vMap, ok := v.(map[string]interface{})
+				if !ok {
+					return errors.Errorf(
+						"Expected value to be a valid metadata object but it's values aren't compatible")
+				}
+				kv := MetadataValue{}
+				if err := kv.FromMap(vMap); err != nil {
+					return errors.Errorf(
+						"Expected value to be a valid metadata object but it was not: %s", err.Error())
+				}
+				val[k] = kv
+			}
+		}
+		m.Value = val
+	default:
+		return errors.Errorf(
+			"%s is not a valid type, expected 'string', 'int', 'float', 'bool', or 'object", m.Type)
+	}
+	return nil
+}
+
+// FromMap tries to make a MetadataValue from a supplied map
+func (m *MetadataValue) FromMap(md map[string]interface{}) error {
+	typI, ok := md["type"]
+	if !ok {
+		return errors.New("Could not make MetadataValue from map, it did not contain a 'type' key")
+	}
+	typ, ok := typI.(string)
+	if !ok {
+		return errors.New("Could not make MetadataValue from map, the type key was not a string as expected")
+	}
+	val, ok := md["value"]
+	if !ok {
+		return errors.New("Could not make MetadataValue from map, it did not contain a 'value' key")
+	}
+	m.Type = typ
+	m.Value = val
+	if err := m.tryCastFields(); err != nil {
+		return errors.Errorf("Could not make MetadataValue from map: %s", err.Error())
+	}
+	return nil
+}
+
+// UnmarshalJSON controls how a MetadataValue is parsed from JSON
+func (m *MetadataValue) UnmarshalJSON(data []byte) error {
+	mv := &struct {
+		Type  string      `json:"type"`
+		Value interface{} `json:"value"`
+	}{}
+	json.Unmarshal(data, &mv)
+	m.Type = mv.Type
+	m.Value = mv.Value
+	return m.tryCastFields()
+}
+
+// Validate validates this MetadataValue
+func (m *MetadataValue) Validate(_ interface{}) error {
+	switch m.Type {
+	case "string":
+		_, ok := m.Value.(string)
+		if !ok {
+			return errors.New("Expected value to be a string but it was not")
+		}
+	case "int":
+		_, ok := m.Value.(int64)
+		if !ok {
+			return errors.New("Expected value to be a int64 but it was not")
+		}
+	case "float":
+		_, ok := m.Value.(float64)
+		if !ok {
+			return errors.New("Expected value to be a float64 but it was not")
+		}
+	case "bool":
+		_, ok := m.Value.(bool)
+		if !ok {
+			return errors.New("Expected value to be a bool but it was not")
+		}
+	case "object":
+		val, ok := m.Value.(Metadata)
+		if !ok {
+			return errors.New("Expected value to be a Metadata but it was not")
+		}
+		err := val.Validate(nil)
+		if err != nil {
+			return errors.Errorf("Metadata value was not valid: %s", err.Error())
+		}
+	default:
+		return errors.Errorf(
+			"%s is not a valid type, expected 'string', 'int', 'float', 'bool', or 'object", m.Type)
+	}
+	return nil
 }
 
 // Metadata stores Metadata for a Manifold resource
-type Metadata map[string]interface{}
+type Metadata map[string]MetadataValue
 
 // MetadataMaxSize defines the max size of the metadata JSON in bytes
-const MetadataMaxSize = 1024
+const MetadataMaxSize = 10 * 1024
 
 // Equals checks the equality of another Metadata against this one
 func (m Metadata) Equals(md Metadata) bool {
-	return dataMapEquals(m, md)
+	if len(m) != len(md) {
+		return false
+	}
+	for k, v := range m {
+		if val, ok := md[k]; !ok || !val.Equals(v) {
+			return false
+		}
+	}
+
+	return true
 }
 
 // Validate validates this Metadata
@@ -46,22 +191,9 @@ func (m Metadata) Validate(_ interface{}) error {
 		if err := Label(k).Validate(nil); err != nil {
 			return errors.Errorf("Key '%s' is not a valid Manifold Label", k)
 		}
-		// Make sure value is an allowed type
-		switch t := v.(type) {
-		case string, bool, float32, float64, int, int8, int16, int32, int64, uint,
-			uint8, uint16, uint32, uint64, json.Number:
-			continue
-		case map[string]interface{}:
-			if err := Metadata(t).Validate(nil); err != nil {
-				return err
-			}
-		case Metadata:
-			if err := t.Validate(nil); err != nil {
-				return err
-			}
-		default:
-			typ := reflect.TypeOf(v)
-			return errors.Errorf("'%s' is an invalid field value type for Metadata", typ.Name())
+		// Make sure value is valid
+		if err := v.Validate(nil); err != nil {
+			return errors.Errorf("Value of key '%s' is not valid: %s", k, err.Error())
 		}
 	}
 	// Make sure total length isn't too long
--- types_test.go
+++ types_test.go
@@ -1,12 +1,12 @@
 package manifold
 
 import (
-	"encoding/json"
+	json "encoding/json"
 	"testing"
 )
 
 func TestFeatureMap(t *testing.T) {
-	t.Run("different FeatureMaps are not equal", func(t *testing.T) {
+	t.Run("Different FeatureMaps are not equal", func(t *testing.T) {
 		a := FeatureMap{
 			"a": 1,
 			"b": "TWO",
@@ -23,13 +23,13 @@ func TestFeatureMap(t *testing.T) {
 		}
 
 		if a.Equals(b) {
-			t.Error("A and B are not equal, but are.")
+			t.Error("Expected A to not eqaul B, but it did.")
 		}
 		if b.Equals(c) {
-			t.Error("B and C are not equal, but are.")
+			t.Error("Expected B to not eqaul C, but it did.")
 		}
 		if a.Equals(c) {
-			t.Error("A and C are not equal, but are.")
+			t.Error("Expected A to not eqaul C, but it did.")
 		}
 	})
 
@@ -46,82 +46,72 @@ func TestFeatureMap(t *testing.T) {
 		}
 
 		if !a.Equals(b) {
-			t.Error("A and B are equal, but aren't.")
+			t.Error("Expected A to eqaul B, but it didn't.")
 		}
 	})
 }
 
 func TestMetadata(t *testing.T) {
-	t.Run("different Metadata are not equal", func(t *testing.T) {
+	t.Run("Different Metadata are not equal", func(t *testing.T) {
 		a := Metadata{
-			"abra":       1,
-			"bulbasaur":  "TWO",
-			"charmander": false,
+			"abra":       {Type: "int", Value: 1},
+			"bulbasaur":  {Type: "string", Value: "TWO"},
+			"charmander": {Type: "bool", Value: false},
 		}
 		b := Metadata{
-			"abra":       2,
-			"bulbasaur":  "one",
-			"charmander": true,
+			"abra":       {Type: "int", Value: 2},
+			"bulbasaur":  {Type: "string", Value: "one"},
+			"charmander": {Type: "bool", Value: true},
 		}
 		c := Metadata{
-			"abra":      1,
-			"bulbasaur": "TWO",
+			"abra":      {Type: "int", Value: 1},
+			"bulbasaur": {Type: "string", Value: "TWO"},
 		}
 
 		if a.Equals(b) {
-			t.Error("A and B are not equal, but are.")
+			t.Error("Expected A to not eqaul B, but it did.")
 		}
 		if b.Equals(c) {
-			t.Error("B and C are not equal, but are.")
+			t.Error("Expected B to not eqaul C, but it did.")
 		}
 		if a.Equals(c) {
-			t.Error("A and C are not equal, but are.")
+			t.Error("Expected A to not eqaul C, but it did.")
 		}
 	})
 
 	t.Run("Metadata with the same values are equal", func(t *testing.T) {
 		a := Metadata{
-			"abra":       1,
-			"bulbasaur":  "TWO",
-			"charmander": false,
+			"abra":       {Type: "int", Value: 1},
+			"bulbasaur":  {Type: "string", Value: "TWO"},
+			"charmander": {Type: "bool", Value: false},
 		}
 		b := Metadata{
-			"abra":       1,
-			"bulbasaur":  "TWO",
-			"charmander": false,
+			"abra":       {Type: "int", Value: 1},
+			"bulbasaur":  {Type: "string", Value: "TWO"},
+			"charmander": {Type: "bool", Value: false},
 		}
 
 		if !a.Equals(b) {
-			t.Error("A and B are equal, but aren't.")
+			t.Error("Expected A to eqaul B, but it didn't.")
 		}
 	})
 
 	t.Run("Expected Metadata is considered valid", func(t *testing.T) {
 		mds := []Metadata{
 			{
-				"abra":       1,
-				"bulbasaur":  "TWO",
-				"charmander": false,
+				"abra":       {Type: "int", Value: int64(1)},
+				"bulbasaur":  {Type: "string", Value: "TWO"},
+				"charmander": {Type: "bool", Value: false},
 			},
 			{
-				"abra":       1,
-				"bulbasaur":  "TWO",
-				"charmander": false,
-				"subdata": Metadata{
-					"abra":       1,
-					"bulbasaur":  "TWO",
-					"charmander": false,
-				},
-			},
-			{
-				"abra":       1,
-				"bulbasaur":  "TWO",
-				"charmander": false,
-				"subdata": map[string]interface{}{
-					"abra":       1,
-					"bulbasaur":  "TWO",
-					"charmander": false,
-				},
+				"abra":       {Type: "int", Value: int64(1)},
+				"bulbasaur":  {Type: "string", Value: "TWO"},
+				"charmander": {Type: "bool", Value: false},
+				"subdata": {Type: "object", Value: Metadata{
+					"abra":       {Type: "int", Value: int64(1)},
+					"bulbasaur":  {Type: "string", Value: "TWO"},
+					"charmander": {Type: "bool", Value: false},
+				}},
 			},
 		}
 
@@ -135,23 +125,18 @@ func TestMetadata(t *testing.T) {
 	t.Run("Expected Metadata is considered invalid", func(t *testing.T) {
 		mds := []Metadata{
 			{
-				"$$$BADLABEL$$": 1,
+				"$$$BADLABEL$$": {Type: "int", Value: int64(1)},
 			},
 			{
-				"bad-value-type": json.Encoder{},
+				"bad-value-type": {Type: "int", Value: "NOTANINT"},
 			},
 			{
-				"nested-value-err": Metadata{
-					"$$$BADLABEL$$": 1,
-				},
+				"nested-value-err": {Type: "object", Value: Metadata{
+					"$$$BADLABEL$$": {Type: "int", Value: int64(1)},
+				}},
 			},
 			{
-				"nested-value-err": map[string]interface{}{
-					"$$$BADLABEL$$": 1,
-				},
-			},
-			{
-				"json-too-big": makeBigString(MetadataMaxSize),
+				"json-too-big": {Type: "string", Value: makeBigString(MetadataMaxSize)},
 			},
 		}
 
@@ -169,4 +154,65 @@ func makeBigString(size int) string {
 		b[i] = 'a'
 	}
 	return string(b)
+}
+
+func TestMetadataValue(t *testing.T) {
+
+	t.Run("UnmarshalJSON is successful when expected", func(t *testing.T) {
+		JSONs := []string{
+			`{ "type": "string", "value": "this is a string" }`,
+			`{ "type": "bool", "value": false }`,
+			`{ "type": "int", "value": 12 }`,
+			`{ "type": "float", "value": 12.2 }`,
+			`{ "type": "object", "value": {  
+				"sub-object-key": { "type": "int", "value": 7 }
+			 } }`,
+		}
+
+		for _, j := range JSONs {
+			mdv := &MetadataValue{}
+			if err := json.Unmarshal([]byte(j), &mdv); err != nil {
+				t.Error(err)
+			}
+			if err := mdv.Validate(nil); err != nil {
+				t.Error(err)
+			}
+		}
+	})
+
+	t.Run("UnmarshalJSON errors when expected", func(t *testing.T) {
+		JSONs := []string{
+			`{ "type": "string", "value": false }`,
+			`{ "type": "bool", "value": 1 }`,
+			`{ "type": "int", "value": false }`,
+			`{ "type": "float", "value": false }`,
+			`{ "type": "object", "value": false }`,
+			`{ "type": "snake", "value": false }`,
+			`{ "type": "object", "value": {  
+				"sub-object-key": false
+			 } }`,
+			`{ "type": "object", "value": {  
+				"sub-object-key": { "value": false }
+			 } }`,
+			`{ "type": "object", "value": {  
+				"sub-object-key": { "type": false, "value": 7 }
+			 } }`,
+			`{ "type": "object", "value": {  
+				"sub-object-key": { "type": "object" }
+			 } }`,
+			`{ "type": "object", "value": {  
+				"sub-object-key": { "type": "snake", "value": false }
+			 } }`,
+		}
+
+		for _, j := range JSONs {
+			mdv := &MetadataValue{}
+			if err := json.Unmarshal([]byte(j), &mdv); err == nil {
+				t.Errorf("Expected err for Unmarshal of: %s", j)
+			}
+			if err := mdv.Validate(nil); err == nil {
+				t.Errorf("Expected err for Validate of: %s", j)
+			}
+		}
+	})
 }
