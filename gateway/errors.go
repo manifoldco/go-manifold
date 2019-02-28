6dcd0e5c9390e25de7eb1a908db76ae18becc88a
Roberto Selbach <r@rst.sh>
2019-02-28 15:23:20 -0500

    Export gateway.Error

--- gateway/errors.go
+++ gateway/errors.go
@@ -2,17 +2,17 @@ package gateway
 
 import (
 	"fmt"
-	"strings"
 )
 
-// gatewayError represents an Error returned by this Middleware to a
-// requestor
-type gatewayError struct {
-	Type     string   `json:"type"`
+// Error represents the error returned by a handler to the requestor
+type Error struct {
+	Type    string `json:"type"`
+	Code    string `json:"code"`
+	Class   string `json:"class"`
 	Message string `json:"message"`
 }
 
-// Error returns the error message represented by this Error
-func (e *gatewayError) Error() string {
-	return fmt.Sprintf("%s: %s", e.Type, e.Message)
+// Error implements the error interface
+func (e *Error) Error() string {
+	return fmt.Sprintf("%s (%s): %s", e.Code, e.Class, e.Message)
 }
